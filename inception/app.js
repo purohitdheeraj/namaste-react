4792f25c9241e79a7dc53ce06445a65a2000bdaa
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2024-12-10 19:33:17 +0530

    parcel is beast

--- inception/app.js
+++ inception/app.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // props can be passed as an object to the createElement function
 // props can be default props or custom props
 
 
 // here hare krishna is also a prop
-const element = React.createElement('h1', {id: 'heading'}, "hare krishna");
+const element = React.createElement('h1', {id: 'heading'}, "hare krisha hare rama");
 
-const parent = React.createElement('div',{id: 'parent'}, React.createElement('div',{id: 'child'}, [React.createElement('h1',{id: 'heading'}, 'i am h1 tag'),React.createElement('h2',{id: 'parent'}, 'i am h2 tag')]) )
+const parent = React.createElement('div',{id: 'parent'}, React.createElement('div',{id: 'child'}, [React.createElement('h1',{id: 'heading'}, 'i am h1 tag'),React.createElement('h2',{id: 'parent'}, 'hare krishna')]) )
 
 console.log(parent)
 
